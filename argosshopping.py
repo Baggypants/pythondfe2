6f3b172b71d57a0c3c75331e39c07f821fac3318
Leon L. Robinson <leon.robinson@qa.com>
2021-09-08 09:17:18 +0100

    argos analogy

---
+++ argosshopping.py
@@ -0,0 +1,13 @@
+# These are not real functions
+
+def argosCounter(stockID):
+    binloc = findTheGoods(stockID)
+    goods = sendPicker(binloc)
+    return goods
+
+
+
+
+paper = argosCatSearch('saucepans with pyrex lids')
+
+carBoot = argosCounter(paper)
